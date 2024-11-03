9052c51c18c8fc7ca1b2dadabbc98009c69b0b77
unknown <jacopswi54@gmail.com>
2024-11-03 15:57:30 +0100

    test analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+    site: 'https://astro-article.netlify.app/',
+    integrations: [
+        tailwind(),
+        mdx(), 
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 });
